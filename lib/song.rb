6420a4faac8ab269a2e24d7cc8fe321176ec2921
Nikolai Pawlowski <nikolaip@gmail.com>
2017-12-07 00:31:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     @genre = genre
     @@count += 1
     @@artists << artist unless @@artists.include?(artist)
-    @@genres << genre unless @@agenres.include?(genre)
+    @@genres << genre unless @@genres.include?(genre)
   end
 
   def self.count
